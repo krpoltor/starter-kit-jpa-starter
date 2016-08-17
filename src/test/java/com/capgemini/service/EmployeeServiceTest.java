3fe7b92b3cc74a2b0add52d72237fc0d11b12e14
krpoltor <krzysztof.poltorak@capgemini.com>
2016-08-17 08:37:21 +0200

    initial tests

---
+++ src/test/java/com/capgemini/service/EmployeeServiceTest.java
@@ -0,0 +1,29 @@
+package com.capgemini.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+
+/**
+ * Test class for testing {@link EmployeeService}<br>
+ * Checklist:<br>
+ * 1. Add employee.<br>
+ * 2. Remove employee<br>
+ * 3. Update employee's data.<br>
+ * 4. Change employee's division.<br>
+ * 5. Find all by name and surname.<br>
+ * 6. Find all by division.<br>
+ * 
+ * @author KRPOLTOR
+ *
+ */
+public class EmployeeServiceTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/com/capgemini/service/ProjectServiceTest.java
@@ -0,0 +1,28 @@
+package com.capgemini.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Test class for testing {@link ProjectService}<br>
+ * Checklist:<br>
+ * 1. Add project.<br>
+ * 2. Remove project.<br>
+ * 3. Update project.<br>
+ * 4. Add employee to project.<br>
+ * 5. Remove employee from project (mark that he has ended working).<br>
+ * 6. Find all employees working on project.<br>
+ * 7. Find all employees working on project longer than N months.<br>
+ * 
+ * @author KRPOLTOR
+ *
+ */
+public class ProjectServiceTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
